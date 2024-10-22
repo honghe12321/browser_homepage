54785ce6fca468739475727c6d27cc06abcef56c
Truimo <xiaomo@20mo.cn>
2024-10-22 11:44:53 +0800

    fix: clock

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,21 +1,22 @@
 import "../css/clock.css"
 import {useEffect, useState} from "react";
-const Clock = ()=>{
-    const [time, setTime] = useState(new Date());
+
+const Clock = () => {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
+            setTime(new Date().toLocaleTimeString());
+        }, 1e3);
 
         return () => {
             clearInterval(interval);
         };
     }, []);
-    return(
+    return (
         <div className="timeBox">
             <div className="clock" id="clock">
-                {time.toLocaleTimeString()}
+                {time}
             </div>
         </div>
     )
