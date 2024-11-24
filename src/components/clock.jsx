4757a5408aee7823a02c76af7e869191e6d1bcfa
honghe12321 <3478327597@qq.com>
2024-11-24 22:33:03 +0800

    fix: 时间格式化

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,11 +1,24 @@
 import "../css/clock.css"
 import {useEffect, useState} from "react";
+function formatDate(date) {
+    // const year = date.getFullYear();
+    // const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    // const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
 
+    // return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+// const now = new Date();
+// console.log(formatDate(now)); // 输出格式化后的当前时间
 const Clock = ({isImmerseFullscreen}) => {
     const [time, setTime] = useState(new Date().toLocaleTimeString());
     useEffect(() => {
         const interval = setInterval(() => {
-            setTime(new Date().toLocaleTimeString());
+            setTime(formatDate(new Date()));
         }, 1e3);
 
         return () => {
