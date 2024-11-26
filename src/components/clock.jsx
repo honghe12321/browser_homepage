ed79a6ec57e66fea9cae50c0a8cfacae8cf63563
Truimo <xiaomo@20mo.cn>
2024-11-26 12:39:16 +0800

    feat: clock optimize

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,6 +1,18 @@
 import "../css/clock.css"
 import {useEffect, useState} from "react";
-function formatDate(date) {
+
+function getCurrentTime() {
+    const date = new Date()
+    const hours = date.getHours().toString().padStart(2, '0')
+    const minutes = date.getMinutes().toString().padStart(2, '0')
+    const seconds = date.getSeconds().toString().padStart(2, '0')
+
+    return {
+        hours, minutes, seconds
+    }
+}
+
+/** function formatDate(date) {
     // const year = date.getFullYear();
     // const month = (date.getMonth() + 1).toString().padStart(2, '0');
     // const day = date.getDate().toString().padStart(2, '0');
@@ -10,29 +22,32 @@ function formatDate(date) {
 
     // return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     return `${hours}:${minutes}:${seconds}`;
-}
+} **/
 
 // const now = new Date();
 // console.log(formatDate(now)); // 输出格式化后的当前时间
 const Clock = ({isImmerseFullscreen}) => {
-    const [time, setTime] = useState(formatDate(new Date()));
+    const [time, setTime] = useState(getCurrentTime());
+
     useEffect(() => {
         const interval = setInterval(() => {
-            setTime(formatDate(new Date()));
+            setTime(getCurrentTime());
         }, 1e3);
 
         return () => {
             clearInterval(interval);
         };
     }, []);
-    return (
 
-            <div className={`${!isImmerseFullscreen ? 'timeBox' : 'timeBoxBig'}`}>
-                <span id="clock">
-                    {time}
-                </span>
-            </div>
-)
+    return (
+        <div className={`${!isImmerseFullscreen ? 'timeBox' : 'timeBoxBig'}`}>
+            <span>{time.hours}</span>
+            <span>:</span>
+            <span>{time.minutes}</span>
+            <span>:</span>
+            <span>{time.seconds}</span>
+        </div>
+    )
 }
 
 export default Clock;
