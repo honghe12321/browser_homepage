299398dfb10dfd90d449e1f0ec5e5bb49d07650d
Truimo <xiaomo@20mo.cn>
2024-11-27 01:57:06 +0800

    fix: 没有背景

--- src/services/BackgroundChanger.jsx
+++ src/services/BackgroundChanger.jsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { BsBox } from "react-icons/bs";
+import { BsBox } from 'react-icons/bs';
 import '../css/home.css'
+
 const BackgroundChanger = () => {
     const [backgroundImage, setBackgroundImage] = useState(null);
-    const [cachedImage, setCachedImage] = useState(null);
-    if ( cachedImage){
-    }
     // 从localStorage中读取缓存的背景图片URL
     useEffect(() => {
         const cachedUrl = localStorage.getItem('backgroundImage');
         if (cachedUrl) {
-            setCachedImage(cachedUrl);
             setBackgroundImage(cachedUrl);
+        } else {
+            setBackgroundImage('https://assets.truimo.com/images/p241022/2f8ef7077540980aaa272e9f2b17e10a.png')
         }
     }, []);
 
@@ -24,20 +23,19 @@ const BackgroundChanger = () => {
                 setBackgroundImage(base64Url);
                 // 将图片URL存储到localStorage中
                 localStorage.setItem('backgroundImage', base64Url);
-                setCachedImage(base64Url);
             };
             reader.readAsDataURL(file);
         }
     };
 
     return (
-        <div>
-            <div id='bg' className='-z-10' style={{ backgroundImage: `url(${backgroundImage})` }}>
-                <label><span className='relative inline-block m-2 p-2 bg-black/20 backdrop-blur rounded-xl'><BsBox size='20' color='white'/></span><input className='hidden' type="file" accept="image/*" onChange={handleImageChange} value=''/></label>
+        <div id='bg' className='-z-10' style={{backgroundImage: `url(${backgroundImage})`}}>
+            <div>
+                <label><span className='relative inline-block m-2 p-2 bg-black/20 backdrop-blur rounded-xl'><BsBox
+                    size='20' color='white'/></span><input className='hidden' type="file" accept="image/*"
+                                                           onChange={handleImageChange} value=''/></label>
             </div>
         </div>
-
-
     );
 };
 
