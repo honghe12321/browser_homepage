a827f0d86d39577858165f53a37e02990594d44d
Truimo <xiaomo@20mo.cn>
2024-11-27 12:10:06 +0800

    feat: 背景图片优化

---
+++ src/libs/base64.js
@@ -0,0 +1,19 @@
+function b64Blob(b64) {
+    const mimeTypeMatch = b64.match(/^data:([^;]+);base64,/)
+    if (mimeTypeMatch) {
+        const mimeType = mimeTypeMatch[1]
+        const data = b64.slice(mimeTypeMatch[0].length)
+        const binaryStr = atob(data)
+        const byteArr = new Uint8Array(binaryStr.length)
+        for (let i = 0; i < binaryStr.length; i++) {
+            byteArr[i] = binaryStr.charCodeAt(i)
+        }
+        const blob = new Blob([byteArr], { type: mimeType })
+        return URL.createObjectURL(blob)
+    }
+    return b64
+}
+
+export {
+    b64Blob
+}
--- src/services/BackgroundChanger.jsx
+++ src/services/BackgroundChanger.jsx
@@ -1,16 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import {BsBox} from 'react-icons/bs';
+import {b64Blob} from '../libs/base64';
 import '../css/home.css'
 
 const BackgroundChanger = () => {
     const [backgroundImage, setBackgroundImage] = useState(null);
+
     // 从localStorage中读取缓存的背景图片URL
     useEffect(() => {
         const cachedUrl = localStorage.getItem('backgroundImage');
         if (cachedUrl) {
-            setBackgroundImage(cachedUrl);
+            setBackgroundImage(b64Blob(cachedUrl));
         } else {
             setBackgroundImage('https://assets.truimo.com/images/p241022/2f8ef7077540980aaa272e9f2b17e10a.png')
+        }
+
+        return () => {
+            URL.revokeObjectURL(backgroundImage);
         }
     }, []);
 
@@ -18,9 +24,9 @@ const BackgroundChanger = () => {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
-            reader.onloadend = () => {
-                const base64Url = reader.result;
-                setBackgroundImage(base64Url);
+            reader.onloadend = (e) => {
+                const base64Url = e.target.result;
+                setBackgroundImage(b64Blob(base64Url));
                 // 将图片URL存储到localStorage中
                 localStorage.setItem('backgroundImage', base64Url);
             };
