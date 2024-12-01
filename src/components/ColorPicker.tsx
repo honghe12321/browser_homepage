e5ff0621767aa23ec53d20dd78ec2b3b675d97cc
Truimo <xiaomo@20mo.cn>
2024-12-02 02:21:21 +0800

    feat: 取色器优化

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,6 +1,7 @@
-import {ChangeEvent, useEffect, useState} from 'react'
+import {ChangeEvent, useEffect, useRef, useState} from 'react'
 import ReactDOM from 'react-dom'
 import {hexToHsl, hslToHex} from '../libs/color'
+import '../css/color-picker.css'
 
 interface HslColor {
     h: number
@@ -13,6 +14,7 @@ interface PickerProps {
 }
 
 function Picker(props: PickerProps) {
+    const containerRef = useRef<HTMLDivElement>(null)
     const [hsl, setHsl] = useState<HslColor>(() => {
 
         const color = localStorage.getItem('themeColor')
@@ -30,8 +32,18 @@ function Picker(props: PickerProps) {
     })
 
     useEffect(() => {
-        const hex = hslToHex(hsl.h, hsl.s, hsl.l);
-        props.onChange(hex)
+        const container = containerRef.current
+        if (container) {
+            container.style.setProperty('--h', `${hsl.h}`)
+            container.style.setProperty('--s', `${hsl.s}%`)
+            container.style.setProperty('--l', `${hsl.l}%`)
+        }
+
+        requestAnimationFrame(() => {
+            const hex = hslToHex(hsl.h, hsl.s, hsl.l);
+            props.onChange(hex)
+        })
+
     }, [hsl])
 
     const onColorChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -43,9 +55,9 @@ function Picker(props: PickerProps) {
     return (
         <div className='fixed inset-0'>
             <div className="md:w-3/5 w-4/5 absolute bottom-5 translate-x-1/2 right-1/2">
-                <div className='backdrop-blur p-5 rounded-md' style={{
+                <div className='backdrop-blur p-5 rounded-md color-picker' style={{
                     animation: 'fade-in 180ms'
-                }}>
+                }} ref={containerRef}>
                     <div className="grid grid-cols-2 items-center py-1">
                         <p>
                             <span>HSL 调色面板</span>
@@ -57,9 +69,7 @@ function Picker(props: PickerProps) {
                             </label>
                         </p>
                     </div>
-                    <div className="color-bar" style={{
-                        backgroundColor: `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`,
-                    }}></div>
+                    <div className="color-bar color-bar0"></div>
                     <div>
                         <label>
                             H（色调）
@@ -89,9 +99,7 @@ function Picker(props: PickerProps) {
                                    }}/>
                         </label>
                     </div>
-                    <div className="color-bar" style={{
-                        background: `linear-gradient(to right, hsl(${hsl.h}, 0%, ${hsl.l}%), hsl(${hsl.h}, 100%, ${hsl.l}%))`,
-                    }}></div>
+                    <div className="color-bar color-bar2"></div>
                     <div>
                         <label>
                             L（亮度）
@@ -106,9 +114,7 @@ function Picker(props: PickerProps) {
                                    }}/>
                         </label>
                     </div>
-                    <div className="color-bar" style={{
-                        background: `linear-gradient(to right, hsl(${hsl.h}, ${hsl.s}%, 0%), hsl(${hsl.h}, ${hsl.s}%, 100%))`,
-                    }}></div>
+                    <div className="color-bar color-bar3"></div>
                 </div>
             </div>
         </div>
