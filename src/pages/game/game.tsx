7d08a5133c391815e1df1bbcc4269e2bb1ca4600
honghe12321 <3478327597@qq.com>
2025-06-11 12:42:01 +0800

    feat: 游戏

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -22,7 +22,7 @@ const games: GameItem[] = [
     {
         title: "换装小游戏",
         img: "https://imgs.crazygames.com/holographic-trends/20210210062727/holographic-trends-cover?metadata=none&quality=85&width=273&fit=crop",
-        url: "https://www.crazygames.com/embed/mean-girls-graduation-day",
+        url: "https://www.crazygames.com/embed/model-wedding---girls-games",
     },
 ];
 
